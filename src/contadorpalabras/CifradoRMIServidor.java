4ed4f476a3d92075fcbf73cf3f13103cddfb193c
Luis Xavier Carcamo Velazquez <luisvelazquez98271@gmail.com>
2023-12-05 00:20:10 -0600

    Add RMI

--- src/contadorpalabras/CifradoRMIServidor.java
+++ src/contadorpalabras/CifradoRMIServidor.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import javax.swing.JTextArea;
 
 public class CifradoRMIServidor extends UnicastRemoteObject implements CifradoRMI {
     public Map<Integer, String[]> resultadosClientes = new HashMap<>();
@@ -22,32 +23,34 @@ public class CifradoRMIServidor extends UnicastRemoteObject implements CifradoRM
     }
 
     @Override
-    public void cifrarPorHilos(String[] texto) throws RemoteException {
-        System.out.println("FUNCION CIFRAR POR HILOS");
-
-            System.out.println(clientes.size());
+    public void contarPalabrasClientes(String[] nombreArchivo, JTextArea log) throws RemoteException {
+        System.out.println("Contar palabras por hilos");
 
         int numClientes = clientes.size();
 
-        int palabrasPorCliente = texto.length / numClientes;
-        int palabrasRestantes = texto.length % numClientes;
+        int palabrasPorCliente = nombreArchivo.length / numClientes;
+        int palabrasRestantes = nombreArchivo.length % numClientes;
         int inicio = 0;
 
         System.out.println("Num clientes: " + numClientes);
         System.out.println("Palabras por cliente: " + palabrasPorCliente);
         System.out.println("Palabras restantes: " + palabrasRestantes);
 
+        long tiempoInicioConHilos = System.currentTimeMillis();
         for (int i = 0; i < numClientes; i++) {
                 int fin = inicio + palabrasPorCliente + (i < palabrasRestantes ? 1 : 0);
-                String[] parteTexto = Arrays.copyOfRange(texto, inicio, fin);
+                String[] parteTexto = Arrays.copyOfRange(nombreArchivo, inicio, fin);
 
                 clientes.get(i+1).receiveMessage(parteTexto, this, i);
 
                 inicio = fin;
             }
+        long tiempoFinConHilos = System.currentTimeMillis();
+        long tiempoTotalConHilos = tiempoFinConHilos - tiempoInicioConHilos;
+        log.append("\nTiempo de ejecución (RMI): " + tiempoTotalConHilos + " ms\n");
     }
     
-    public CifradoRMI connect(String Ip, CifradoRMIServidor server) {
+    public CifradoRMI connect(String Ip, CifradoRMIServidor server, JTextArea log) {
         try {
             LocateRegistry.createRegistry(9000);
 
@@ -56,7 +59,7 @@ public class CifradoRMIServidor extends UnicastRemoteObject implements CifradoRM
 
             java.rmi.Naming.rebind("rmi://" + Ip + ":9000/CifradoRMI", service);
 
-            System.out.println("Servidor de RMI listo.");
+            log.append("\nServidor de RMI listo.\n");
             return service;
 
         } catch (Exception e) {
@@ -65,18 +68,18 @@ public class CifradoRMIServidor extends UnicastRemoteObject implements CifradoRM
         return null;
     }
 
-@Override
-    public void registerClient(ClientCallback cliente) throws RemoteException {
-    contadorClientes++;
-    clientes.put(contadorClientes, cliente);
-    
-    // Imprimir el array de clientes
-    System.out.println("Clientes registrados: " + Arrays.toString(clientes.keySet().toArray()));
-    
-    System.out.println("Cliente registrado: " + cliente.toString() + " - ID: " + contadorClientes);
-    
-}
-    
+    @Override
+        public void registerClient(ClientCallback cliente) throws RemoteException {
+        contadorClientes++;
+        clientes.put(contadorClientes, cliente);
+
+        // Imprimir el array de clientes
+        System.out.println("Clientes registrados: " + Arrays.toString(clientes.keySet().toArray()));
+
+        System.out.println("Cliente registrado: " + cliente.toString() + " - ID: " + contadorClientes);
+
+    }
+        
     @Override
     public void sendResult(String[] resultado, int clientId) {
         resultadosClientes.put(clientId, resultado);
@@ -91,20 +94,7 @@ public class CifradoRMIServidor extends UnicastRemoteObject implements CifradoRM
                 // Agrega los elementos del resultado del cliente a la lista
                 Collections.addAll(finalTextList, resultadoCliente);
                 // Procesa el resultado como desees
-                System.out.println("Resultado del Cliente " + i + ": " + Arrays.toString(resultadoCliente));
-            }
-
-            // Convierte la lista a un array
-            String[] finalText = finalTextList.toArray(new String[0]);
-
-            int count = 0;
-    
-            for (int i = 0; i < finalText.length; i++) {
-                hill.escribirArchivo(finalText[i]);
-                count++;
-                if (count % 10 == 0) {
-                    hill.escribirArchivo("\n");
-                }
+                //System.out.println("Resultado del Cliente " + i + ": " + Arrays.toString(resultadoCliente));
             }
         }
     }
